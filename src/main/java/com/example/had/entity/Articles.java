477969f6a0d93c1df7fd30b31787915bcdbc1470
sirhashir <umairhashir89@gmail.com>
2023-04-04 01:21:55 +0530

    predata creation

--- src/main/java/com/example/had/entity/Articles.java
+++ src/main/java/com/example/had/entity/Articles.java
@@ -68,12 +68,6 @@ public class Articles {
     private String articleTitle;
 
 
-//    @Column(
-//            name = "article_data",
-//            nullable = false
-//    )
-//    private String articleData;
-
     public Articles() {
     }
 
